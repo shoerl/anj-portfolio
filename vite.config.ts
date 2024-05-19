3249b4f35d5106b4e3f6be5a79aea5df3b127205
shoerl <hoerl.s@northeastern.edu>
2024-05-19 19:54:14 -0400

    Workflow fixes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/anj-portfolio/',
   plugins: [
     react(),
     tsconfigPaths(),
